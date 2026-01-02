d2ad6b226204eb9d1142a2c68a3f24813a0cae2b
thathanathip679 <thanathip.nan@gmail.com>
2026-01-02 15:20:34 +0700

    lab12_2.cpp

--- lab12_2.cpp
+++ lab12_2.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int x, int y);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x, int y){
+    int z;
+    if(x%y == 0) return y;
+    else {
+        z = gcd(y,x%y);
+        return z;
+    }
 }
