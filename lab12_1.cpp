4ed826f4702c2c60a3e9f12c73aa1090a01b1014
thathanathip679 <thanathip.nan@gmail.com>
2026-01-02 15:20:21 +0700

    lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+#include<ctime>
 using namespace std;
 
+unsigned long long int fibonacci(unsigned long long int x);
+
+
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
+
+unsigned long long int fibonacci(unsigned long long int x){
+    if (x==0) return 0;
+    else if (x==1) return 1;
+    else if(x >= 0) {
+        unsigned long long int y = fibonacci(x-1) + fibonacci(x-2);
+        return y;
+    }
+}
+
